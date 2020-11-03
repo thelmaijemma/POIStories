f29d02b3a21ac13b48d2f1e9a82216b997474c81
Thelma Ijemma <68825395+thelmaijemma@users.noreply.github.com>
2020-11-03 01:35:06 +0000

    Update index.js
    responsive mobile design

--- index.js
+++ index.js
@@ -1,193 +1,107 @@
-
-// INITIATING THE MAP 
-function isIconMouseEvent(e) {
-  return "placeId" in e;
+body {
+  color: #fffeeb;
+  background-image: url('https://images.pexels.com/photos/2908984/pexels-photo-2908984.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260');
 }
 
-function initMap() {
-      const origin = { lat: 51.497518, lng:  -0.134912 };
-      const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
-        center: origin,
-        mapTypeId: "roadmap",
+.instruction, .submit, .results{
+  display: flex;
+  flex-direction: column;
+}
+
+#map {
+  height: 37%;
+  width: 90%;
+  margin: auto;
+}
+
+ #results{
+  width: 75%;
+  margin: auto;
+}
+
+.instruction{
+  color: #fffeeb;
+  text-align: center;
+  font-size: 40pt;
+  width: 80%;
+  margin: auto;
+}
+
+#results{
+  background-image: url('https://images.pexels.com/photos/326311/pexels-photo-326311.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+  color: #fffeeb;
+  text-align: center;
+  padding-inline: 5%;
+  padding-bottom: 15%;
+  padding-top: 5%;
+  margin-top: 5%;
+}
 
 
-      });
+#pac-input {
+  background-color: #fffeeb;
+  font-family: Roboto;
+  font-size: 25px;
+  font-weight: 300;
+  margin-left: 12px;
+  padding: 0 11px 0 13px;
+  text-overflow: ellipsis;
+  width: 400px;
+}
 
-// SEARCH BOX GENERATOR - GOOGLE CODE
-const input = document.getElementById("pac-input");
-const searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", () => {
-    searchBox.setBounds(map.getBounds());
-  });
+#pac-input:focus {
+  border-color: #4d90fe;
+}
 
-  let markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
-     // For each place, get the icon, name and location.
-     const bounds = new google.maps.LatLngBounds();
-     places.forEach((place) => {
-       if (!place.geometry) {
-         console.log("Returned place contains no geometry");
-         return;
-       }
-       const icon = {
-         url: place.icon,
-         size: new google.maps.Size(71, 71),
-         origin: new google.maps.Point(0, 0),
-         anchor: new google.maps.Point(17, 34),
-         scaledSize: new google.maps.Size(25, 25),
-       };
-       // Create a marker for each place.
-       markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-    });
- 
-
-
-// INITIATE POI LISTENER 
-map.addListener("click", function(event) {
-placesService = new google.maps.places.PlacesService(map);
-if (isIconMouseEvent(event)) {
-  console.log("You clicked on place:" + event.placeId);
-  
-  const pid = event.placeId;
-
-  render(placesService, pid);
-
+button {
+  padding:0.7em 1.7em;
+  border-radius:10px;
+  font-family:'Roboto',sans-serif;
+  font-size: xx-large;
+  color:#fffeeb;
+  background-color:brown;
+  box-shadow:inset 0 -0.6em 1em -0.35em rgba(0,0,0,0.17),inset 0 0.6em 2em -0.3em rgba(255,255,255,0.15),inset 0 0 0em 0.05em rgba(255,255,255,0.12);
+  width: 40%;
   }
 
-});
+.submit{
+  display: block;
+  text-align: center;
+  padding-top: 5%;
+  padding-bottom: 1%;
 }
 
-
-
-// FORMAT PLACE ID AND POI CLICK EVENT LISTENER 
-function render(service, placeiden){
-  $('button').click(function(){
-
-    const request = {
-      placeId: placeiden,
-      fields: ['address_components','formatted_address','name','vicinity']
-    };
-          
-          service.getDetails(request, callback);
-          function callback(place, status) {
-            let placeName = place.name;
-            let placeAddress = place.formatted_address;
-            console.log("here is the address: "+ placeAddress);
-           if (!place.vicinity){
-             prepareAltSearch(placeName, placeAddress);
-           } else {
-            let placeVicinity = place.vicinity;
-            console.log("this is the vicinity: " + placeVicinity);
-            const city = prepareVicinity (placeVicinity);
-            console.log("this is the city: " + city);
-            prepareSearch(placeName, city);
-           }
-         
-          }
-  
-             }
-            );
-            }
-
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-            infoWindow.setPosition(pos);
-            infoWindow.setContent(
-              browserHasGeolocation
-                ? "Error: The Geolocation service failed."
-                : "Error: Your browser doesn't support geolocation."
-            );
-            infoWindow.open(map);
-          }
-          
-          
-
-
-// functions to prepare raw place information from google api 
-/// --> format for NYT fetch request URL
-function prepareVicinity(vicinity){
-const vicinityArray = vicinity.split(/[ ,]+/);
-console.log(vicinityArray);
-return vicinityArray[vicinityArray.length-1];
-}
-            
-function prepareSearch(name, cityname){
-console.log("we did a regular search");
-const searchArray = name.split(/[ ,']+/);
-const searchName = searchArray.join('_');
-fetchURL(searchName,cityname)
+.headline{
+  font-size: x-large;
+  text-emphasis: bold;
 }
 
-function prepareAltSearch(name, address){
-console.log("time to do an alt search");
-const altSearchAddress = prepareAddressForAltSearch(address);
-fetchURL(name,altSearchAddress);
+.year{
+  font-size: x-large;
+}
+
+#results a {
+  text-decoration: none;
+  color: #fffeeb;
+  background-color: brown;
+  font-size: large;
+}
+
+#map {
+  height: 40%;
+  width: 90%;
+  margin: auto;
+}
+
+@media (min-width: 992px){
+  #map {
+    height: 40%;
+    width: 70%;
+    margin: auto;
   }
 
-function prepareAddressForAltSearch(address){
-const addressArray = address.split(/[',']+/);
-console.log("here is the address array: " + addressArray);
-const altVicinity = addressArray[0];
-const altCity = addressArray[1];
-const altSearchArrayRaw = [altVicinity, altCity];
-return altSearchArrayRaw;
+  .instruction{
+    font-size: xxx-large;
+    width: 60%;
   }
-
-
-
-// factory function to create a custom fetch request given click inputs 
-function fetchURL(name, item2){
-console.log(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${name}_${item2}&sort=oldest&api-key=KtYlaYp2oCUKHAhuNPmxTfftZnAUnGbU`);
-fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${name}_${item2}&sort=oldest&api-key=KtYlaYp2oCUKHAhuNPmxTfftZnAUnGbU`)
-  .then(response => response.json())
-  .then(responseJson => 
-    displayResults(responseJson))
-    }
-
-function displayResults(responseJson){
-$('#results').empty();
-  // formatting note: responseJson.response.docs[i]abstract
-  for(let i=0; i< 10; i++){
-    if(!responseJson.response.docs[i].headline.main == false){
-      console.log(responseJson.response.docs[i].headline.main);
-      const yearRaw = responseJson.response.docs[i].pub_date;
-      const year = yearRaw.substring(0,4);
-        $('#results').append(`<p class="headline">Headline: ${responseJson.response.docs[i].headline.main}</p><p>URL: <a href="${responseJson.response.docs[i].web_url}">${responseJson.response.docs[i].web_url}</a></p><p class="year">YEAR: ${year}</p>`)
-          } else if (!responseJson.response.docs[i].headline.main){
-          console.log("article with no headline")
-          }
-        }
-        }
-      
-          
+}
